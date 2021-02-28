5f93ca5b79318a9b3fafaca85060e750a9c8d239
Taylor Mitchell <taylor.j.mitchell@gmail.com>
2021-02-28 04:19:34 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request
+from replit import db, database
+import datetime
+import json
+
+app = Flask('app')
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+@app.route('/get')
+def get_db():
+  return notedb.to_json()
+
+@app.route('/submit')
+def submit():
+  note = request.args['note']
+  tags = request.args.getlist('tags')
+  notedb.save_note(note, tags=tags)
+  return render_template('index.html')
+
+class NoteDB:
+  def __init__(self, db_url=None):
+    self.db = database.Database(db_url) if db_url else db
+
+  def to_json(self):
+    return json.dumps(dict(db.items()))
+
+  def get_latest(self):
+    return db["notes"][-1]
+
+  def save_note(self, note, tags=[]):
+    timestamp = int(datetime.datetime.now().timestamp())
+    notes = db.get("notes", [])
+    notes.append({
+      "timestamp": timestamp,
+      "note": note,
+      "tags": tags
+    })
+    db["notes"] = notes
+
+  def delete_all(self):
+    for k in db.keys():
+      del db[k]
+
+notedb = NoteDB()
+
+if __name__=="__main__":  
+  app.run(host='0.0.0.0', port=8080)
